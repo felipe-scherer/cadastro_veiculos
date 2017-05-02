6b8f726a24bea5c602c635a2ae00930af4e758fe
scherer <scherer@scherer.elipse.com.br>
2017-05-02 13:37:05 -0300

    config bd tests -> h2

--- src/test/java/br/com/felipe_scherer/repository/TestVeiculoRepository.java
+++ src/test/java/br/com/felipe_scherer/repository/TestVeiculoRepository.java
@@ -6,8 +6,6 @@ import javax.persistence.EntityManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
